e52a0275cce4f0d3efb9a03cd926f306b02662ab
Charles Duan <cduan@sbf5.com>
2021-03-27 04:47:18 -0400

    Working on proxy cards

--- proxy/sets.rb
+++ proxy/sets.rb
@@ -44,17 +44,34 @@ class PokemonTcgApi
     end
   end
 
+  ENERGY_IDS = {
+    1 => 'sm1-164', # Grass
+    2 => 'sm1-165', # Fire
+    3 => 'sm1-166', # Water
+    4 => 'sm1-167', # Lightning
+    5 => 'sm1-168', # Psychic
+    6 => 'sm1-169', # Fighting
+    7 => 'sm1-170', # Darkness
+    8 => 'sm1-171', # Metal
+    9 => 'sm1-172', # Fairy
+  }
+
   #
   # Retrieves the API card ID given a PTCGO identifier of expansion code and
   # card number.
   #
   def card_id(expansion_code, number)
 
-    # Special case because Shining Fates has two number series so code SHF shows
-    # up twice in the sets database
-    if expansion_code == 'SHF'
+    case expansion_code
+
+    when 'SHF'
+      # Special case because Shining Fates has two number series so code SHF
+      # shows up twice in the sets database
       return "swsh45sv-#{number}" if number.start_with?("SV")
       return "swsh45-#{number}"
+
+    when 'Energy'
+      return ENERGY_IDS[number.to_i]
     end
 
     expansion_code = {
@@ -62,7 +79,7 @@ class PokemonTcgApi
       'SMP' => 'PR-SM',
     }[expansion_code] || expansion_code
 
-    sets.find { |s| s["ptcgoCode"] == expansion_code }
+    set = sets.find { |s| s["ptcgoCode"] == expansion_code }
     raise "Unknown set #{expansion_code}" unless set
     return "#{set["id"]}-#{number}"
   end
@@ -78,7 +95,7 @@ class PokemonTcgApi
       return File.open(filename) do |io| JSON.parse(io.read) end
     else
       card_data = request("cards/#{id}")["data"]
-      open(File.join(@options.card_dir, "#{id}.json"), "w") do |io|
+      open(filename, "w") do |io|
         io.write(JSON.pretty_generate(card_data))
       end
       return card_data
@@ -88,9 +105,186 @@ class PokemonTcgApi
 
 end
 
+#
+# Constructs the TeX file for the proxy cards.
+#
 class TeXCardBuilder
   def initialize(options)
     @options = options
+    @api = @options.api
+
+    #
+    # Keeps count of the cards added to this print.
+    #
+    @card_count = 0
+    @cards = {
+      'Pokémon' => {},
+      'Trainer' => {},
+      'Energy' => {},
+    }
+
+    #
+    # Buffer of text to add to the document after the preamble.
+    #
+    @buffer = ""
+
+    @name = "[New Deck]"
+    @description = "[Description goes here]"
+  end
+
+  attr_accessor :name, :description
+
+  def add_card(quantity, expansion, number)
+    data = @api.card_data(expansion, number)
+
+    @card_count += quantity
+
+    if @cards[data['supertype']][data]
+      @cards[data['supertype']][data] += quantity
+    else
+      @cards[data['supertype']][data] = quantity
+    end
+
+    return if @options.no_cards
+    quantity.times do
+      case data['supertype']
+      when 'Pokémon' then add_pokemon(data)
+      when 'Trainer' then add_trainer(data)
+      when 'Energy' then add_energy(data)
+      end
+    end
+  end
+
+  TYPE_CODES = {
+    'Grass' => 'G',
+    'Fire' => 'R',
+    'Water' => 'W',
+    'Lightning' => 'L',
+    'Psychic' => 'P',
+    'Fighting' => 'F',
+    'Darkness' => 'D',
+    'Metal' => 'M',
+    'Fairy' => 'F',
+    'Dragon' => 'N',
+    'Colorless' => 'C',
+  }
+
+  def encode_types(array)
+    array.map { |x| TYPE_CODES[x] }.join("")
+  end
+
+  def encode_wr(array)
+    res = ""
+    if array
+      array.each do |hash|
+        res << "\\do{#{TYPE_CODES[hash['type']]}}{#{hash['value']}}"
+      end
+    end
+    return res
+  end
+
+  def text(str)
+    if str && str.length > 40
+      "%\n#{str}%\n"
+    else
+      "#{str}"
+    end
+  end
+
+  def get_picture(data)
+    outfile = File.join(@options.image_dir, data['id'] + ".jpg")
+    return outfile if File.exist?(outfile)
+    URI.open(data['images']['large']) do |io|
+      cmd = [ 'convert', '-', '-crop', '620x385+57+98', outfile ]
+      puts cmd.join(" ")
+      IO.popen(cmd, 'w') do |pio|
+        pio.write(io.read)
+      end
+    end
+    return outfile
+  end
+
+  def add_pokemon(data)
+    @buffer << "\\pokemoncard{%\n"
+    @buffer << "\\nameline{#{data['subtypes'][0]}}{#{data['name']}}" \
+      "{#{data['hp']}}{#{encode_types(data['types'])}}\n"
+
+    evolve = "#{data['evolvesFrom']}" if data['evolvesFrom']
+    subtypes = data['subtypes'][1..-1].reject { |x| data['name'].include?(x) }
+    subtypes = subtypes.map { |x| "\\do{#{x}}" }.join
+    if evolve || !subtypes.empty?
+      @buffer << "\\subtitleline{#{evolve}}{#{subtypes}}\n"
+    end
+
+    @buffer << "\\imageline{#{get_picture(data)}}\n"
+
+    if data['abilities']
+      data['abilities'].each do |ability|
+        @buffer << "\\ability{#{ability['type']}}{#{ability['name']}}" \
+          "{#{text(ability['text'])}}\n"
+      end
+    end
+
+    if data['attacks']
+      data['attacks'].each do |attack|
+        @buffer << "\\attack{#{encode_types(attack['cost'])}}" \
+          "{#{attack['name']}}" \
+          "{#{attack['damage']}}{#{text(attack['text'])}}\n"
+      end
+    end
+
+    @buffer << "\\wrrline{#{encode_wr(data['weaknesses'])}}" \
+      "{#{encode_wr(data['resistances'])}}" \
+      "{#{encode_types(data['retreatCost'])}}\n"
+
+    if data['rules']
+      data['rules'].each do |rule|
+        @buffer << "\\ruleline{#{text(rule)}}\n"
+      end
+    end
+
+    @buffer << "\\cardid{#{data['set']['ptcgoCode']}}{#{data['number']}}\n"
+    @buffer << "}\n"
+  end
+
+  def add_trainer(data)
+  end
+
+
+  def add_energy(data)
+  end
+
+  def add_summary
+    if @card_count != 60
+      warn("Expected 60 cards, but deck has #@card_count")
+    end
+
+    @buffer << "\\summarycard{#@name}{#@description}{%\n"
+    @cards.each do |type, list|
+      @buffer << "\\summaryhead{#{type}}\n"
+      list.each do |data, quantity|
+        name = data['name'].gsub(/\(.*\)/, '')
+
+        @buffer << "\\summaryline{#{quantity}}{#{name}}"
+        if data['supertype'] == 'Energy' && data['subtypes'].include?('Basic')
+          @buffer << "{}{}\n"
+        else
+          @buffer << "{#{data['set']['ptcgoCode']}}{#{data['number']}}\n"
+        end
+      end
+    end
+    @buffer << "}\n"
+  end
+
+  def write_file(io)
+    #open(File.join(@options.templates_dir, @options.tex_preamble)) do |pio|
+    #  io.write(pio.read)
+    #end
+    io.write(
+      "\\input{#{File.join(@options.templates_dir, @options.tex_preamble)}}\n"
+    )
+    io.write(@buffer)
+    io.write("\\end{document}\n")
   end
 end
 
@@ -100,7 +294,8 @@ class Executor
 
   def initialize(options)
     @api = PokemonTcgApi.new(options)
-    @texer = TexCardBuilder.new(options)
+    options.api = @api
+    @texer = TeXCardBuilder.new(options)
     @options = options
   end
 
@@ -139,13 +334,37 @@ class Executor
     p data
   end
 
+  def doc_deck ; "Produce a proxy deck sheet" end
+  def cmd_deck(file = nil)
+    if file
+      open(file) do |io| read_deck(io) end
+    else
+      read_deck(STDIN)
+    end
+    @texer.add_summary
+    open(@options.tex_output, 'w') do |io|
+      @texer.write_file(io)
+    end
+  end
+  def read_deck(io)
+    io.each do |line|
+      if line =~ /^(\d+)\s(?:.*\s)?(\w+)\s+(\w+)\s*$/
+        @texer.add_card($1.to_i, $2, $3)
+      else
+        warn("Could not parse line: #{line}")
+      end
+    end
+  end
 end
 
 options = OpenStruct.new(
   :sets_file => "sets.json",
   :set_logo_dir => "set-logos",
   :card_dir => "cards",
-  :templates_dir = "tex_templates",
+  :image_dir => "images",
+  :templates_dir => "tex_templates",
+  :tex_preamble => "preamble.tex",
+  :tex_output => "deck.tex",
 )
 Executor.new(options).run(*ARGV)
 
